e2f0eb7482d0433e1820bbc35aae3d12068d2ff4
XanderAtalay <asatalay@mgh.harvard.edu>
2024-03-22 10:52:54 -0400

    reformatted id_fields.json and, reduced number of inputs, added docstrings to scrub functions.

--- check_dicoms.py
+++ check_dicoms.py
@@ -11,13 +11,13 @@ import argparse
 import pydicom as dcm
 
 
-# In[1]:
+# In[2]:
 
 
-def check_dicoms(session_path, dicom_field_config):
+def check_dicoms(session_path):
     print(f'Checking {session_path} for DICOMs...')
-    # Load scrub field configurations from JSON file
-    with open(dicom_field_config, 'r') as json_file:
+    # Load scrub field configurations from JSON id_fields config file
+    with open('./id_fields.json', 'r') as json_file:
         scrub_field_dict = json.load(json_file)
 
     dicom_output_dict = {}
@@ -27,18 +27,15 @@ def check_dicoms(session_path, dicom_field_config):
     num_dicoms = 0
     # Iterate through all files and subdirectories within the session path
     for path, subdirs, files in os.walk(session_path):
-        # Iterate through files ending with '.dcm'
         for file in [dicom for dicom in files if dicom.endswith('.dcm')]:
             num_dicoms += 1
-            # Read DICOM file
             dicom_data = dcm.dcmread(os.path.join(path, file))
             # Iterate through fields in the scrub_field_dict and print values
-            for field_name, field_tag in scrub_field_dict.items():
-                # Convert hexadecimal values to integers
+            for field_tag, field_name in scrub_field_dict.items():
+                # Convert hexadecimal values to integers and check for field in the DICOM header
                 x, y = map(lambda val: int(val, base=16), field_tag.split(","))
-                # Check if field exists in DICOM data
                 if (x, y) in dicom_data:
-                    # Obtain the field value:
+                    # Obtain the field value and append if new:
                     dicom_output_dict[field_name].add(dicom_data[(x, y)].value)
 
     print(f'{num_dicoms} DICOMs found: \n')
@@ -53,15 +50,13 @@ def check_dicoms(session_path, dicom_field_config):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Check DICOM fields.')
-    parser.add_argument('-p', '--session_directory', default='.', help='Path to the session directory containing DICOM files')
-    parser.add_argument('-c', '--dicom_field_config', default='./scrub_fields.json', help='Path to the JSON file containing DICOM field configurations')
+    parser.add_argument('scan_session_directory', nargs='?', default='.', help='Path to the directory containing DICOM files')
 
     # Parse arguments
     args = parser.parse_args()
 
     # Extract arguments
     session_directory = args.session_directory
-    dicom_field_config = args.dicom_field_config
 
     check_dicoms(session_directory, dicom_field_config)
 
--- scrub_dicoms.py
+++ scrub_dicoms.py
@@ -17,17 +17,31 @@ import pydicom as dcm
 
 
 def vr_scrub(vr):
+    """
+    Provides de-identified value for DICOM fields based on Value Representations (VRs).
+
+    Args:
+        vr (str): The DICOM Value Representation (VR) of the data.
+
+    Returns:
+        str, int, float, or bytes: Redacted or replaced value based on the VR.
+            - For string types (LO, SH, PN, LT, ST, UT, DA, TM, DT, CS, UI), returns 'REDACTED'.
+            - For integer types (IS, SL, SS, UL, US), returns 0.
+            - For decimal types (DS, FD, FL), returns 0.0.
+            - For other byte types (OB, OW, UN), returns bytes('REDACTED', 'utf-8').
+    """
+    
     if vr in ["LO", "SH", "PN", "LT", "ST", "UT", "DA", "TM", "DT", "CS", "UI"]:
-        # The VR is a string type, so convert the new value to a string
+        # The VR is a string type
         return('REDACTED')
     elif vr in  ["IS", "SL", "SS", "UL", "US"]:
-        # The VR is an integer type, so convert the new value to an integer
+        # The VR is an integer type
         return(0)
     elif vr in ["DS", "FD", "FL"]:
-        # The VR is a decimal type, so convert the new value to a float
+        # The VR is a decimal type
         return(0.0)
     elif vr in ["OB", "OW", "UN"]:
-        # The VR is other byte, so convert the new value to bytes
+        # The VR is other byte
         return(bytes('REDACTED', 'utf-8'))
     else:
         print(f"The tag {tag} has VR {vr}, which is not handled by this function.")
@@ -55,14 +69,13 @@ def remove_identifiers_from_dicom(dicom_file, subject_id=None):
     filename = dicom_file.split('/')[-1]
     new_filename = modality + "." + seriesInstanceUID + "." + instanceNumber + "." + ''.join(random.choices(string.ascii_uppercase + string.digits, k=6)) + ".dcm"
     
-    # Iterate through fields to scrub, specified by the scrub_fields.json file
+    # Iterate through fields to scrub, specified by the id_fields.json file
     for field in scrub_fields:
         # Convert hexadecimal values to integers
         x, y = map(lambda val: int(val, base=16), field.split(","))
     
         # Check if field exists in DICOM data
         if (x, y) in dicom_data:
-            # Get value representation of the field
             value_rep = dicom_data[(x, y)].VR
             # Scrub the value
             dicom_data[(x, y)].value = vr_scrub(value_rep)
@@ -80,6 +93,13 @@ def remove_identifiers_from_dicom(dicom_file, subject_id=None):
 
 
 def scrub_dicoms(session_path, subject_id=None):
+    """
+    Removes identifying information from all DICOM files in a directory and optionally sets a new subject ID.
+
+    Args:
+        session_path (str): Path to the scan session / directory containing DICOM files.
+        subject_id (str, optional): New subject ID to assign. Defaults to None.
+    """
     dicom_number = 0
     
     # Iterate through all files and subdirectories within the session path
@@ -101,23 +121,21 @@ def scrub_dicoms(session_path, subject_id=None):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Scrub identifying information from DICOM files.')
-    parser.add_argument('-p', '--session_directory', default='.', help='Path to the session directory containing DICOM files')
+    parser.add_argument('scan_session_directory', nargs='?', default='.', help='Path to the directory containing DICOM files')
     parser.add_argument('-id', '--subject_id', default=None, help='Subject ID to assign to DICOM header')
-    parser.add_argument('-c', '--dicom_field_config', default='./scrub_fields.json', help='Path to the JSON file containing fields to scrub')
 
-    # Parse arguments
+    # Parse and extract arguments
     args = parser.parse_args()
 
-    # Extract arguments
     session_directory = args.session_directory
     subject_id = args.subject_id
     dicom_field_config = args.dicom_field_config
 
     # Load scrub field configurations from JSON file
-    with open(dicom_field_config, 'r') as json_file:
+    with open('./id_fields.json', 'r') as json_file:
         scrub_field_dict = json.load(json_file)
     
-    scrub_fields = list(scrub_field_dict.values())
+    scrub_fields = list(scrub_field_dict.keys())
 
     # Scrub DICOM files
     scrub_dicoms(session_path=session_directory, subject_id=subject_id)
